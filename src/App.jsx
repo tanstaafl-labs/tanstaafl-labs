c12afbd942b8a12083e11722ea29857c8e2fe5ef
boilerrat <128boilerrat@gmail.com>
2024-02-10 01:32:24 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>TANSTAAFL Labs.</h1>
+        <p>Web3 Innovation Consulting</p>
+        <a href="mailto:tanstaafl-labs@proton.me">tanstaafl-labs@proton.me</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
